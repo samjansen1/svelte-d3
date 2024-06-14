05353eed51c198002d6f048ed735987b5a4e31f2
samjansen1 <sam.jansen001@gmail.com>
2024-06-14 10:41:21 +0100

    initial commit

---
+++ udemy/schwarzmuller/1-getting-started/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	let name = 'Sam';
+	let age = 35;
+
+	function increaseAge() {
+		age++;
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}, my age is {age}</h1>
+<button on:click={increaseAge}>Increase Age</button>
